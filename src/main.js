4b4e8a8fec5ea78639b72f64aa7e9a97f25d89ec
Vedant <vshetty.vs@gmail.com>
2020-01-16 20:29:46 +0530

    Removed wordlist from main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
-
+// let WordList = ['grit', 'creativity', 'impact', 'diversity', 'trust']
 new Vue({
   render: h => h(App)
 }).$mount("#app");
+
