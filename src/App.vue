cb9394f580b26bf9ece290c9b799467099c5886e
Vedant <vshetty.vs@gmail.com>
2020-01-16 20:18:06 +0530

    Added "How To Play Instructions"

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <div id="app">
+    <h1>Hangman</h1>
+    <div class ='instructions'>
+      <h2>How to play?</h2>
+      <p> Type the letters into your keyboard to guess the word. But Remember! 
+      Every incorrect letter brings you closer to losing the game and your head...</p>
+    </div>
+
+    <div class = "columns">
+      <div class = "hiddenWord">
+        <span v-if="isGameOver"> {{word}}</span>
+        <span v-else> {{hiddenWord}}</span>
+      </div>
+    
+    <!-- Add Image -->
+    </div>
+
+
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
